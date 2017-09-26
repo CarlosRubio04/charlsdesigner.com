eb0cf9d3cae9f70b880b774eef186406983a6459
carlos Rubio <carlos@iMac-de-carlos.local>
2017-09-25 21:25:02 -0500

    home changes

--- blocks/home.php
+++ blocks/home.php
@@ -1,17 +1,36 @@
-<section class="main">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-12">
-				<h1 class="text-center">
-					¡ Hello World !
-				</h1>
-			</div>
-			<div class="col-12">
-				<h2 class="text-center">Mi nombre es Carlos Rubio</h2>
-			</div>
-			<div class="col-12">
-				<h3 class="text-center">Frontend devoloper en Bogotá, Colombia</h3>
-			</div>
-		</div>
-	</div>
-</section>
+<div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Cover</h3>
+              <nav class="nav nav-masthead">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Contact</a>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Cover your page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Cover template for <a href="https://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
