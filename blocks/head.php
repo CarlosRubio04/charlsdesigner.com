0089b6ab00e4948141af6ae8b922f7486b0f5cd9
Carlos Rubio Mac BM <carlosr@3dm.com.co>
2017-09-26 10:06:14 -0500

    Final working page

--- blocks/head.php
+++ blocks/head.php
@@ -7,6 +7,9 @@
       <title>Carlos Rubio | Frontend Developer</title>
 
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      
+      <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700,900" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
       <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
       <!--[if lt IE 9]>
--- blocks/home.php
+++ blocks/home.php
@@ -1,16 +1,41 @@
 <section class="main">
 	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-12">
-				<h1 class="text-center">
-					¡ Hello World !
-				</h1>
+		<div class="row">
+			<div class="col">
+				<img src="img/function.png" alt="My Function">
 			</div>
-			<div class="col-12">
-				<h2 class="text-center">Mi nombre es Carlos Rubio</h2>
-			</div>
-			<div class="col-12">
-				<h3 class="text-center">Frontend devoloper en Bogotá, Colombia</h3>
+		</div>
+		<div class="row">
+			<div class="col">
+				<h1>Carlos Rubio <small>Frontend Developer <i class="fa fa-code" aria-hidden="true"></i></small></h1>
+				<h4><i class="fa fa-map-marker" aria-hidden="true"></i>  Bogotá Colombia</h4>
+				<ul class="social">
+					<li>
+						<a href="https://www.facebook.com/charlsdesigners/" target="blank">
+							<i class="fa fa-facebook-square" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://twitter.com/CharlsDesigner" target="blank">
+							<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://instagram.com/CharlsDesigner" target="blank">
+							<i class="fa fa-instagram" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.behance.net/carlosrubiodeveloper" target="blank">
+							<i class="fa fa-behance" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/CarlosRubio04/" target="blank">
+							<i class="fa fa-github" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
