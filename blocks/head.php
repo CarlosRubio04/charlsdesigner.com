3411a72d9aff636f60e65524fefbf14ac3ab29d8
Carlos Rubio Mac BM <carlosr@3dm.com.co>
2018-01-24 17:40:34 -0500

    Analytics, keywords

--- blocks/head.php
+++ blocks/head.php
@@ -1,21 +1,54 @@
 <head>
-     <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
-      <meta name="description" content="Frontend Developer, Desarollador web en Bogotá, Colombia">
-      <meta name="author" content="Carlos Rubio">
-      <title>Carlos Rubio | Frontend Developer</title>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THVWM9H');</script>
+<!-- End Google Tag Manager -->
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+ <meta name="description" content="Frontend Developer, Desarollador web en Bogotá, Colombia">
+ <meta name="keywords" content="desarrollador freelance bogota,aplicaciones moviles, aplicaciones moviles bogota, aplicaciones moviles colombia, sitios web bogota, paginas web bogota, paginas web colombia, sitios web colombia, joomla bogota, desarrollador freelance, diseñador web, programador apps, desarrrollo apps, aplicaciones moviles para empresas, personas que desarrollan aplicaciones moviles, diseño web bogota, desarrollo apps, empresas que desarrollan apps colombia, desarrollador independiente bogota, desarrollador web, desarrollador web bogota, desarrollador frontend, desarrollador angular, campañas de marketing" />
+ <meta name="author" content="Carlos Rubio">
+ <title>Carlos Rubio | Frontend Developer</title>
 
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-      
-      <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700,900" rel="stylesheet">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+ <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700,900" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+ <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-      <!-- Custom CSS -->
-      <link href="css/app.css" rel="stylesheet">
+<![endif]-->
+<!-- Custom CSS -->
+<link href="css/app.css" rel="stylesheet">
+
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-113018676-1', 'auto');
+    ga('send', 'pageview');
+
+
+    ga('set', 'title', 'Prueba GA')
+
+    ga('set', 'dimension1', 'Free');
+
+    ga('send', 'pageview', {
+      'dimension1': 'Free',
+    });
+
+    ga('send', 'event', 'Pagina', 'Scroll', 'http://charlsdesigner.com', {
+      'dimension1': 'Free',
+    });
+  </script>
+
 </head>
--- index.php
+++ index.php
@@ -4,18 +4,20 @@
 <?php require_once('blocks/head.php'); ?>
 <body>
 <!-- Google Tag Manager (noscript) -->
-
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THVWM9H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-    <?php require_once('blocks/header.php'); ?>
-    <?php if(!isset($_REQUEST['content'])){
-      require_once('blocks/home.php');
-    }elseif($_REQUEST['content']=='gracias'){
-      require_once('blocks/gracias.php');
-    }else{
-      require_once('blocks/404.php');
-    } 
-    ?>
-    <?php require_once('blocks/footer.php'); ?>
+
+  <?php require_once('blocks/header.php'); ?>
+  <?php if(!isset($_REQUEST['content'])){
+    require_once('blocks/home.php');
+  }elseif($_REQUEST['content']=='gracias'){
+    require_once('blocks/gracias.php');
+  }else{
+    require_once('blocks/404.php');
+  } 
+  ?>
+  <?php require_once('blocks/footer.php'); ?>
   <!-- All js -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.js"></script>
